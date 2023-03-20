41fef8a711017a52c3a32caefe6a5eb46935db33
Kamila Pantoja IPDEC <kamila.pantoja@ipdec.org>
2023-03-20 13:21:54 -0400

    feat: javascript com microsserviços

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('prefixo')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() //prefixo
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('teste')
+  acao() {
+    return 'Full Cycle';
   }
 }
